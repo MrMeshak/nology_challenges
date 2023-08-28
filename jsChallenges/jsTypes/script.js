4cdc55b05640eab35a535868a1ddbfa0cbc30b82
MrMeshak <94204153+MrMeshak@users.noreply.github.com>
2023-08-28 10:53:20 +1000

    Added html and js for jsTypes challenge

---
+++ jsChallenges/jsTypes/script.js
@@ -0,0 +1,12 @@
+console.log(typeof 15); // Prediction: number
+console.log(typeof 5.5); // Prediction: number
+console.log(typeof NaN); // Prediction: number
+console.log(typeof 'hello'); // Prediction: string
+console.log(typeof true); // Prediction: boolean
+console.log(typeof (1 != 2)); // Prediction: boolean
+console.log('hamburger' + 's'); // Prediction: "hamburgers"
+console.log('hamburgers' - 's'); // Prediction: error -> Not a Number
+console.log('1' + '3'); // Prediction: '13'
+console.log('1' - '3'); // Prediction: Number -2
+console.log('johnny' - 5); // Prediction: error -> not a number
+console.log(99 * 'luftbaloons'); // Prediction: error -> not a number
